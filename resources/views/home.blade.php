1801f61da8978eb5c88cdf8b4d7c9d1249d56b4c
Rakasimp <rakaimbang@gmail.com>
2026-03-25 11:43:42 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Temukan Kopi</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    @include('components.hero')
+
+</body>
+</html>
